8382fcc68a5f77f18adcf621fe04deb12d994982
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-01-02 00:57:19 -0300

    encoding/json: Use `json.Unmarshal` instead of the decoder
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- encoding/json/parse.go
+++ encoding/json/parse.go
@@ -3,6 +3,7 @@ package json
 import (
 	"encoding/json"
 	"io"
+	"io/ioutil"
 )
 
 func Parse(data []byte) (value interface{}, err error) {
@@ -11,7 +12,12 @@ func Parse(data []byte) (value interface{}, err error) {
 }
 
 func ParseReader(r io.Reader) (value interface{}, err error) {
-	err = json.NewDecoder(r).Decode(&value)
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &value)
 	return value, err
 }
 
@@ -21,7 +27,12 @@ func ParseObject(data []byte) (value map[string]interface{}, err error) {
 }
 
 func ParseObjectReader(r io.Reader) (value map[string]interface{}, err error) {
-	err = json.NewDecoder(r).Decode(&value)
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &value)
 	return value, err
 }
 
@@ -31,16 +42,11 @@ func ParseArray(data []byte) (value []map[string]interface{}, err error) {
 }
 
 func ParseArrayReader(r io.Reader) (value []map[string]interface{}, err error) {
-	err = json.NewDecoder(r).Decode(&value)
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, &value)
 	return value, err
-}
-
-func StringMarshal(v interface{}) (s string, err error) {
-	b, err := json.Marshal(v)
-	return string(b), err
-}
-
-func StringUnmarshal(s string) (i interface{}, err error) {
-	err = json.Unmarshal([]byte(s), &i)
-	return i, err
 }
