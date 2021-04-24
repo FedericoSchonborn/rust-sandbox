2086ee7ec9b8095f180e715d66d198af232f352e
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-04-23 22:36:51 -0300

    or: Support multiple defaults
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- or/or.go
+++ or/or.go
@@ -2,17 +2,23 @@ package or
 
 import "reflect"
 
-func Or(v, d interface{}) interface{} {
+func Or(v interface{}, defaults ...interface{}) interface{} {
 	value := reflect.ValueOf(v)
-	dType := reflect.TypeOf(d)
-
-	if dType != value.Type() {
-		panic("Type mismatch")
-	}
 
 	if !value.IsZero() {
 		return v
 	}
 
-	return d
+	for _, d := range defaults {
+		dvalue := reflect.ValueOf(d)
+		if dvalue.Type() != value.Type() {
+			panic("Type mismatch")
+		}
+
+		if !dvalue.IsZero() {
+			return d
+		}
+	}
+
+	return nil
 }
