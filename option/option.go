d167c1f243f8925a334ffe041810b0a5dabc6115
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-08-24 21:29:19 -0300

    `option`: Update for Go tip and use a pointer for the value
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

---
+++ option/option.go
@@ -0,0 +1,123 @@
+package option
+
+import (
+	"github.com/fdschonborn/go-sandbox/tuple"
+	"github.com/fdschonborn/go-sandbox/zero"
+)
+
+type Option[T any] struct {
+	value *T
+}
+
+func Some[T any](value T) Option[T] {
+	return Option[T]{value: &value}
+}
+
+// None and Option{} are equivalent.
+func None[T any]() Option[T] {
+	return Option[T]{value: nil}
+}
+
+func (o Option[T]) IsSome() bool {
+	return o.value != nil
+}
+
+func (o Option[T]) IsNone() bool {
+	return o.value == nil
+}
+
+func (o Option[T]) Unwrap() T {
+	if o.value == nil {
+		panic("Called Option.Unwrap on None value")
+	}
+
+	return *o.value
+}
+
+func (o Option[T]) UnwrapOr(def T) T {
+	if o.value == nil {
+		return def
+	}
+
+	return *o.value
+}
+
+func (o Option[T]) UnwrapOrElse(fn func() T) T {
+	if o.value == nil {
+		return fn()
+	}
+
+	return *o.value
+}
+
+func Map[T, U any](o Option[T], fn func(T) U) Option[U] {
+	if o.value == nil {
+		return None[U]()
+	}
+
+	return Some(fn(*o.value))
+
+}
+
+func (o Option[T]) OkOr(err error) (T, error) {
+	if o.value == nil {
+		return zero.Zero[T](), err
+	}
+
+	return *o.value, nil
+}
+
+func (o Option[T]) OkOrElse(fn func() error) (T, error) {
+	if o.value == nil {
+		return zero.Zero[T](), fn()
+	}
+
+	return *o.value, nil
+}
+
+func (o Option[T]) Filter(pred func(T) bool) Option[T] {
+	if o.value == nil {
+		return None[T]()
+	}
+
+	if !pred(*o.value) {
+		return None[T]()
+	}
+
+	return Some(*o.value)
+}
+
+func (o Option[T]) Or(ob Option[T]) Option[T] {
+	if o.value == nil {
+		return ob
+	}
+
+	return o
+}
+
+func (o Option[T]) OrElse(fn func() Option[T]) Option[T] {
+	if o.value == nil {
+		return fn()
+	}
+
+	return o
+}
+
+func Zip[A, B any](a Option[A], b Option[B]) Option[tuple.Tuple2[A, B]] {
+	if a.value != nil && b.value != nil {
+		return Some(tuple.Tuple2[A, B]{
+			A: *a.value,
+			B: *b.value,
+		})
+	}
+
+	return None[tuple.Tuple2[A, B]]()
+}
+
+func Flatten[T any](o Option[Option[T]]) Option[T] {
+	if o.value == nil || *o.value == None[T]() {
+		return None[T]()
+	}
+
+	return *o.value
+}
