3dbdd88932113ca63f4eb46537a81bddc7191969
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-08-24 21:28:05 -0300

    `or`: Use type parameters
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- or/or.go
+++ or/or.go
@@ -1,24 +1,18 @@
 package or
 
-import "reflect"
+import "github.com/fdschonborn/go-sandbox/zero"
 
-func Or(v interface{}, defaults ...interface{}) interface{} {
-	value := reflect.ValueOf(v)
-
-	if !value.IsZero() {
-		return v
+func Or[T comparable](value T, defaults ...T) T {
+	zero := zero.Zero[T]()
+	if value != zero {
+		return value
 	}
 
-	for _, d := range defaults {
-		dvalue := reflect.ValueOf(d)
-		if dvalue.Type() != value.Type() {
-			panic("Type mismatch")
-		}
-
-		if !dvalue.IsZero() {
-			return d
+	for _, value := range defaults {
+		if value != zero {
+			return value
 		}
 	}
 
-	return nil
+	return zero
 }
