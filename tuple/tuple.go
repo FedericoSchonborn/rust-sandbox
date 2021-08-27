cb2e34482dd7c43ca90dcb44c2ad4b2dfd45d087
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-08-26 23:41:51 -0300

    `tuple`: Add `NewTuple*` functions
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- tuple/tuple.go
+++ tuple/tuple.go
@@ -5,6 +5,10 @@ type Tuple2[A, B any] struct {
 	B B
 }
 
+func NewTuple2[A, B any](a A, b B) Tuple2[A, B] {
+	return Tuple2[A, B]{A: a, B: b}
+}
+
 func (t2 Tuple2[A, B]) Unpack() (A, B) {
 	return t2.A, t2.B
 }
@@ -13,6 +17,10 @@ type Tuple3[A, B, C any] struct {
 	A A
 	B B
 	C C
+}
+
+func NewTuple3[A, B, C any](a A, b B, c C) Tuple3[A, B, C] {
+	return Tuple3[A, B, C]{A: a, B: b, C: c}
 }
 
 func (t3 Tuple3[A, B, C]) Unpack() (A, B, C) {
