8cf59e26499810ee15ab6d6085a84754a13c3a1c
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-08-26 22:46:55 -0300

    `zero`: Add `IsZero`
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- zero/zero.go
+++ zero/zero.go
@@ -5,3 +5,8 @@ func Zero[T any]() T {
 	var zero T
 	return zero
 }
+
+// IsZero reports wether a value of type T is the zero value of type T.
+func IsZero[T comparable](value T) bool {
+	return value == Zero[T]()
+}
