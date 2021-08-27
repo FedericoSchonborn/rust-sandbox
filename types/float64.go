2c12b7c9f7401159598f942a4b03e84353968c64
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-08-26 22:46:31 -0300

    `types`: Remove
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- types/float64.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "math"
-
-type Float64 float64
-
-func (f Float64) Pow(y Float64) Float64 {
-	return Float64(math.Pow(float64(f), float64(y)))
-}
--- types/int.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "math"
-
-type Int int
-
-func (i Int) Pow(y Int) Int {
-	return Int(math.Pow(float64(i), float64(y)))
-}
