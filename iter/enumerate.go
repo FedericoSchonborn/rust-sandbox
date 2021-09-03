bb8ea7b79b0912ee2b70c91e6c48cc622ca8fc6c
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-09-03 00:10:54 -0300

    iter: Add `Enumerate`
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

---
+++ iter/enumerate.go
@@ -0,0 +1,29 @@
+package iter
+
+import (
+	"github.com/fdschonborn/go-sandbox/option"
+	"github.com/fdschonborn/go-sandbox/tuple"
+)
+
+type enumerate[Item any, Iter Iterator[Item]] struct {
+	iter Iter
+	n    int
+}
+
+func Enumerate[Item any, Iter Iterator[Item]](iter Iter) Iterator[tuple.Tuple2[int, Item]] {
+	return &enumerate[Item, Iter]{
+		iter: iter,
+		n:    0,
+	}
+}
+
+func (e *enumerate[Item, Iter]) Next() option.Option[tuple.Tuple2[int, Item]] {
+	item, ok := Next[Item, Iter](e.iter)
+	if !ok {
+		return option.None[tuple.Tuple2[int, Item]]()
+	}
+
+	result := tuple.NewTuple2(e.n, item)
+	e.n += 1
+	return option.Some(result)
+}
