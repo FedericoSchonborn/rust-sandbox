8cf00601e71152486b7395ca7701e31c19eae54d
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-09-03 00:11:48 -0300

    iter: Add examples
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- iter/iter_test.go
+++ iter/iter_test.go
@@ -4,21 +4,50 @@ import (
 	"fmt"
 
 	"github.com/fdschonborn/go-sandbox/iter"
-	"github.com/fdschonborn/go-sandbox/tuple"
 )
 
-func Example() {
-	inner := map[string]int{
-		"banana": 42,
-		"apple":  38,
-		"orange": 96,
+func ExampleFind() {
+	a := []int{1, 2, 3}
+
+	equals := func(value int) func(int) bool {
+		return func(item int) bool {
+			return item == value
+		}
 	}
 
-	iter.ForEach(
-		iter.MapIterator(inner), func(tuple tuple.Tuple2[string, int]) {
-			key, value := tuple.Unpack()
-
-			fmt.Println(key, value)
-		},
-	)
+	fmt.Println(iter.Find(iter.SliceIterator(a), equals(2)))
+	fmt.Println(iter.Find(iter.SliceIterator(a), equals(5)))
+	// Output:
+	// Some(2)
+	// None
 }
+
+func ExampleFold() {
+	a := []int{1, 2, 3}
+	sum := iter.Fold(iter.SliceIterator(a), 0, func(acc int, item int) int {
+		return acc + item
+	})
+
+	fmt.Println(sum)
+	// Output:
+	// 6
+}
+
+/* NOTE: Broken
+func ExampleMap() {
+	a := []int{1, 2, 3}
+	iter := iter.Map(iter.SliceIterator(a), func(item int) int {
+		return 2 * item
+	})
+
+	fmt.Println(iter.Next())
+	fmt.Println(iter.Next())
+	fmt.Println(iter.Next())
+	fmt.Println(iter.Next())
+	// Output:
+	// Some(2)
+	// Some(4)
+	// Some(6)
+	// None
+}
+*/
