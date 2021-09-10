1fc342097edf26565d5e38e62b430f42d4162f14
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-09-10 20:31:19 -0300

    zero: Add `Defaulter`/`Default`
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- zero/zero.go
+++ zero/zero.go
@@ -1,5 +1,18 @@
 package zero
 
+type Defaulter[Self any] interface {
+	Default() Self
+}
+
+func Default[T any]() T {
+	var zero T
+	if d, ok := (interface{})(zero).(Defaulter[T]); ok {
+		return d.Default()
+	}
+
+	return zero
+}
+
 // Zero returns the zero value of type T.
 func Zero[T any]() T {
 	var zero T
