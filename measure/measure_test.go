4791d08b2da1f58d2c9aa587ea2e14d0277e97ae
Federico Damián Schonborn <fdschonborn@gmail.com>
2021-10-11 19:21:35 -0300

    measure: Fix test
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- measure/measure_test.go
+++ measure/measure_test.go
@@ -8,7 +8,7 @@ import (
 
 func Example() {
 	m := measure.M(42.0)
-	fmt.Printf("%f meters are %f kilometers", m, m.Km())
+	fmt.Printf("%.1f meters are %.3f kilometers", m, m.Km())
 
 	// Output:
 	// 42.0 meters are 0.042 kilometers
