42268109c1d10b2332cc80d8a2d24adacb9655ed
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-03-02 01:37:42 -0300

    task: New package
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

---
+++ go/task/task.go
@@ -0,0 +1,50 @@
+package task
+
+import "sync"
+
+type Handle[T any] struct {
+	done   chan bool
+	result T
+}
+
+func Join[T any](handles ...*Handle[T]) []T {
+	var wg sync.WaitGroup
+	var m sync.Mutex
+	results := make([]T, len(handles))
+
+	for _, handle := range handles {
+		wg.Add(1)
+
+		go func(handle *Handle[T]) {
+			result := handle.Join()
+
+			m.Lock()
+			defer m.Unlock()
+			results = append(results, result)
+
+			wg.Done()
+		}(handle)
+	}
+
+	wg.Wait()
+	return results
+}
+
+func Spawn[T any](fn func() T) *Handle[T] {
+	done := make(chan bool)
+	var result T
+	go func() {
+		result = fn()
+		done <- true
+	}()
+
+	return &Handle[T]{
+		done:   done,
+		result: result,
+	}
+}
+
+func (h *Handle[T]) Join() T {
+	<-h.done
+	return h.result
+}
