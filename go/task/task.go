80d6cb9df970faf59c15d1346b6aeb748948871e
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-03-02 23:32:39 -0300

    task: Make function argument fallible
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- go/task/task.go
+++ go/task/task.go
@@ -2,45 +2,67 @@ package task
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 type Handle[T any] struct {
-	done   chan bool
-	result T
+	done  chan bool
+	value T
+	err   error
 }
 
-func Join[T any](handles ...*Handle[T]) []T {
+func Join[T any](handles ...*Handle[T]) ([]T, error) {
 	var (
-		wg      sync.WaitGroup
-		results = make([]T, len(handles))
+		wg     sync.WaitGroup
+		mu     sync.Mutex
+		values []T
+		err    error
+		stop   int32
 	)
 
-	for n, handle := range handles {
+	for _, handle := range handles {
 		wg.Add(1)
 
-		go func(handle *Handle[T], results []T, n int) {
-			results[n] = handle.Join()
-			wg.Done()
-		}(handle, results, n)
+		go func(handle *Handle[T]) {
+			defer wg.Done()
+
+			if atomic.LoadInt32(&stop) == 1 {
+				return
+			}
+
+			value, _err := handle.Join()
+			if _err != nil {
+				mu.Lock()
+				err = _err
+				mu.Unlock()
+
+				atomic.StoreInt32(&stop, 1)
+				return
+			}
+
+			mu.Lock()
+			values = append(values, value)
+			mu.Unlock()
+		}(handle)
 	}
 
 	wg.Wait()
-	return results
+	return values, err
 }
 
-func New[T any](fn func() T) *Handle[T] {
+func New[T any](fn func() (T, error)) *Handle[T] {
 	handle := &Handle[T]{
 		done: make(chan bool),
 	}
 
 	go func() {
-		handle.result = fn()
+		handle.value, handle.err = fn()
 		handle.done <- true
 	}()
 	return handle
 }
 
-func (h *Handle[T]) Join() T {
+func (h *Handle[T]) Join() (T, error) {
 	<-h.done
-	return h.result
+	return h.value, h.err
 }
--- go/task/task_test.go
+++ go/task/task_test.go
@@ -9,23 +9,27 @@ import (
 )
 
 func Example() {
-	waitOneSecondAndReturnFive := func() int {
+	waitOneSecondAndReturnFive := func() (int, error) {
 		time.Sleep(1 * time.Second)
-		return 5
+		return 5, nil
+	}
+	handle := task.New(waitOneSecondAndReturnFive)
+
+	result, err := handle.Join()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	handle := task.New(waitOneSecondAndReturnFive)
-	result := handle.Join()
 	fmt.Println(result)
-
 	// Output:
 	// 5
 }
 
 func ExampleJoin() {
 	rand.Seed(time.Now().Unix())
-	returnSix := func() int {
-		return 6
+	returnSix := func() (int, error) {
+		return 6, nil
 	}
 
 	handles := make([]*task.Handle[int], 10)
@@ -33,9 +37,13 @@ func ExampleJoin() {
 		handles[i] = task.New(returnSix)
 	}
 
-	results := task.Join(handles...)
-	fmt.Println(results)
+	array, err := task.Join(handles...)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	fmt.Println(array)
 	// Output:
 	// [6 6 6 6 6 6 6 6 6 6]
 }
